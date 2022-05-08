06a8362fecfb0936463e6bf4bb3faee08b491c69
CAUmang <103166884+CAUmang@users.noreply.github.com>
2022-05-08 09:24:02 +0530

    Time: 36 ms (73.28%), Space: 13.9 MB (71.98%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)%2:
+            return False
+        mylist =[]
+        d = {")":"(", "}":"{", "]":"["}
+        for i in s:
+            if i in d.values():
+                mylist.append(i)
+            elif len(mylist) and d[i] == mylist[-1]:
+                mylist.pop()
+            else:
+                return False
+        if len(mylist) == 0:
+            return True
+        else:
+            return False
+
+        
