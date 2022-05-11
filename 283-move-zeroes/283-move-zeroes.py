533c8c29d1f29de10036ad0188145f5ca60c0a72
CAUmang <103166884+CAUmang@users.noreply.github.com>
2022-05-11 22:27:16 +0530

    Time: 234 ms (50.01%), Space: 15.5 MB (66.95%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,16 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # j = 0
+        # number = 0
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         nums[j] = nums[i]
+        #         j += 1
+        #     else:
+        #         number += 1
+        # k = -1
+        # for n in range(number):
+        #     nums[k] = 0
+        #     k -= 1
+        
         j = 0
-        number = 0
         for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[j] = nums[i]
-                j += 1
+            if nums[j] == 0:
+                nums += [nums.pop(j)]
             else:
-                number += 1
-        k = -1
-        for n in range(number):
-            nums[k] = 0
-            k -= 1
+                j += 1
+                
+            
         
