f2d3a73c9f6ecb14ef39a79668fdf88d64c68839
CAUmang <103166884+CAUmang@users.noreply.github.com>
2022-05-19 00:11:39 +0530

    Time: 84 ms (53.05%), Space: 13.8 MB (60.48%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -4,14 +4,35 @@ class Solution:
         rev = []
         a = -1
         y = ""
-        nums = [str(i) for i in str(x)]
-        for k in range(len(nums)):
-            rev.append(nums[a])
-            a -= 1
-        for j in rev:
-            y += str(j)
+        if x<0:
+            return False
         
-        if str(x) ==str(y):
-            return True
-        else:
-            False
+        while x>=10:
+            nums.append(x%10)
+            x = x//10
+        nums.append(x)
+        rev = nums[::-1]
+        
+        for i in range(len(nums)):
+            if nums[i] != rev[i]:
+                return False
+        return True
+        
+        # print(nums)
+        # print(nums[::-1])
+#         nums = [str(i) for i in str(x)]
+                    
+#         for k in range(len(nums)):
+#             rev.append(nums[a])
+#             a -= 1
+#         for j in rev:
+#             y += str(j)
+
+
+
+#         if str(x) ==str(y):
+#             return True
+#         else:
+#             False
+
+        
