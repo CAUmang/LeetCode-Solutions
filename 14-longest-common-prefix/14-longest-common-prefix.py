1a8d594b94833bb88b8a5c72221602586bf77712
CAUmang <103166884+CAUmang@users.noreply.github.com>
2022-05-21 11:36:57 +0530

    Time: 43 ms (63.62%), Space: 13.9 MB (88.60%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,33 +1,92 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        def left(s, amount):
-            return s[:amount]
+        n = len(strs)
+        if n==1:
+            return strs[0]
+        minimum_length = float('inf')
+        for s in strs:
+            minimum_length = min(minimum_length, len(s))
+        if not minimum_length:
+            return ""
+        mini_str = min(strs)
+        longest_prefix = 0
+        flag = False
+        for i in range(minimum_length):
+            ith_letter =  mini_str[i]
+            for j in range(n):
+                curr = strs[j]
+                if curr[i] != ith_letter:
+                    flag = True
+                    break
+            if flag:
+                break
+            else:
+                longest_prefix += 1
+        return mini_str[:longest_prefix]
+                
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     def left(s, amount):
+#             return s[:amount]
         
-        short = float("inf")
-        for n in strs:
-            short = min(len(n), short)
-        print(short)
-        # print(len(strs))
+#         short = float("inf")
+#         for n in strs:
+#             short = min(len(n), short)
+#         print(short)
+#         # print(len(strs))
         
-        common = left(strs[0], short)
-        print(left(common, short))
-        k=0
-        if short > 0:
+#         common = left(strs[0], short)
+#         print(left(common, short))
+#         k=0
+#         if short > 0:
             
-            for n in range(401):
-                if short == 0:
-                    common = ""
-                    return common
-                else:
-                    if left(common, short) == left(strs[k],short):
-                        common = left(strs[k], short)
-                        k += 1
-                        if k >= len(strs):
-                            return common
-                            break
-                    else:
-                        short -= 1
+#             for n in range(401):
+#                 if short == 0:
+#                     common = ""
+#                     return common
+#                 else:
+#                     if left(common, short) == left(strs[k],short):
+#                         common = left(strs[k], short)
+#                         k += 1
+#                         if k >= len(strs):
+#                             return common
+#                             break
+#                     else:
+#                         short -= 1
 
-        else:
-            common = ""
-            return common
+#         else:
+#             common = ""
+#             return common
